30a5e1939c7207b9878fe0e31c6faea345ee02d2
AndreaAldana <65413878+AndreaAldana@users.noreply.github.com>
2020-05-29 17:35:37 -0400

    Continuación de proyecto y ejercicios con array

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,7 +1,16 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter cells: ");
+        String enter = sc.next();
+        System.out.println("---------");
+        System.out.println("| " + enter.charAt(0) + " "+ enter.charAt(1) + " "+enter.charAt(2) +" |");
+        System.out.println("| " + enter.charAt(3) + " "+ enter.charAt(4) + " "+enter.charAt(5) +" |");
+        System.out.println("| " + enter.charAt(6) + " "+ enter.charAt(7) + " "+enter.charAt(8) +" |");
+        System.out.println("---------");
     }
 }
